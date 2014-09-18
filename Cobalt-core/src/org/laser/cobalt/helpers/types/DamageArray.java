97dc851c9951f5d51be9c804af1a4dfd0e91d090
Mobile Tree <masse.wayne@gmail.com>
2014-09-18 01:25:21 -0400

    starting damage array type

---
+++ Cobalt-core/src/org/laser/cobalt/helpers/types/DamageArray.java
@@ -0,0 +1,22 @@
+package org.laser.cobalt.helpers.types;
+
+public class DamageArray {
+
+	protected float physical, fire, water, earth, lightning;
+
+	public DamageArray() {
+		physical = 0;
+		fire = 0;
+		water = 0;
+		earth = 0;
+		lightning = 0;
+	}
+	
+	public void addDamage(Damage damageType, float amount){
+		
+	}
+	
+	private void modifyPhysical(float amount){
+		
+	}
+}
