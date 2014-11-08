f46b274874a4fda4e8c4aad730eaff5fddeccf7b
Wayne Masse <masse.wayne@gmail.com>
2014-11-08 09:05:25 -0500

    quest.java

--- Cobalt-core/src/org/laser/cobalt/gameobjects/Quest.java
+++ Cobalt-core/src/org/laser/cobalt/gameobjects/Quest.java
@@ -1,14 +1,21 @@
 package org.laser.cobalt.gameobjects;
 
+import org.laser.cobalt.helpers.types.Reward;
+
 public class Quest {
-	
+
 	private int questId;
-	
+
 	public Quest(int id) {
-		this.id = id;
+		this.questId = id;
 	}
-	
+
 	public Reward generateReward() {
-		
+		Reward retVal = new Reward();
+		return retVal;
+	}
+
+	public int getQuestId() {
+		return questId;
 	}
 }
